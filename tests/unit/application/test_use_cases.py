5e69434420c257f9c2913cc7e68d34cb7f7834d7
David Seddon <david.seddon@octoenergy.com>
2019-02-24 10:30:49 +0000

    Make use case test use class

--- tests/unit/application/test_use_cases.py
+++ tests/unit/application/test_use_cases.py
@@ -9,47 +9,48 @@ from tests.adapters.printing import FakePrinter
 from tests.helpers.contracts import AlwaysPassesContract
 
 
-def test_check_contracts_and_print_report():
-    settings.configure(
-        USER_OPTION_READER=FakeUserOptionReader(),
-        GRAPH_BUILDER=FakeGraphBuilder(),
-        PRINTER=FakePrinter(),
-    )
-    settings.USER_OPTION_READER.set_options(
-        UserOptions(
-            root_package_name='mypackage',
-            contracts=(
-                AlwaysPassesContract(name='Contract foo'),
-                AlwaysPassesContract(name='Contract bar'),
+class TestCheckContractsAndPrintReport:
+    def test_all_successful(self):
+        settings.configure(
+            USER_OPTION_READER=FakeUserOptionReader(),
+            GRAPH_BUILDER=FakeGraphBuilder(),
+            PRINTER=FakePrinter(),
+        )
+        settings.USER_OPTION_READER.set_options(
+            UserOptions(
+                root_package_name='mypackage',
+                contracts=(
+                    AlwaysPassesContract(name='Contract foo'),
+                    AlwaysPassesContract(name='Contract bar'),
+                )
             )
         )
-    )
-    settings.GRAPH_BUILDER.set_graph(
-        FakeGraph(
-            root_package_name='mypackage',
+        settings.GRAPH_BUILDER.set_graph(
+            FakeGraph(
+                root_package_name='mypackage',
+            )
         )
-    )
 
-    result = check_contracts_and_print_report()
+        result = check_contracts_and_print_report()
 
-    settings.PRINTER.pop_and_assert(
-        """
-        =============
-        Import Linter
-        =============
+        settings.PRINTER.pop_and_assert(
+            """
+            =============
+            Import Linter
+            =============
+    
+            ---------
+            Contracts
+            ---------
+    
+            Analyzed 23 files, 44 dependencies.
+            -----------------------------------
+    
+            Contract foo KEPT
+            Contract bar KEPT
+    
+            Contracts: 2 kept, 0 broken.
+            """
+        )
 
-        ---------
-        Contracts
-        ---------
-
-        Analyzed 23 files, 44 dependencies.
-        -----------------------------------
-
-        Contract foo KEPT
-        Contract bar KEPT
-
-        Contracts: 2 kept, 0 broken.
-        """
-    )
-
-    assert result == SUCCESS
+        assert result == SUCCESS
