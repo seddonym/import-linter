41cfd6fc49f50d51f80198d77accc6e97b1f7f50
David Seddon <david.seddon@octoenergy.com>
2019-03-02 12:02:33 +0000

    Add rendering test for independence contract

--- tests/unit/contracts/test_independence.py
+++ tests/unit/contracts/test_independence.py
@@ -1,8 +1,11 @@
 import pytest
 
 from importlinter.contracts.independence import IndependenceContract
+from importlinter.application.app_config import settings
+from importlinter.domain.contract import ContractCheck
 
 from tests.adapters.graph import FakeGraph
+from tests.adapters.printing import FakePrinter
 
 
 @pytest.mark.parametrize(
@@ -117,3 +120,93 @@ def test_independence_contract(shortest_chains, invalid_chains):
         assert absolute_invalid_chains == contract_check.metadata['invalid_chains']
     else:
         assert contract_check.kept
+
+
+def test_render_broken_contract():
+    settings.configure(
+        PRINTER=FakePrinter(),
+    )
+    contract = IndependenceContract(
+        name='Independence contract',
+        session_options={
+            'root_package_name': 'mypackage',
+        },
+        contract_options={
+            'modules': [
+                'mypackage.blue',
+                'mypackage.green',
+                'mypackage.yellow',
+            ],
+        },
+    )
+    check = ContractCheck(
+        kept=False,
+        metadata={
+            'invalid_chains': [
+                {
+                    'upstream_module': 'mypackage.yellow',
+                    'downstream_module': 'mypackage.blue',
+                    'chains': [
+                        [
+                            {
+                                'importer': 'mypackage.blue.foo',
+                                'imported': 'mypackage.utils.red',
+                                'line_numbers': (16, 102),
+                            },
+                            {
+                                'importer': 'mypackage.utils.red',
+                                'imported': 'mypackage.utils.brown',
+                                'line_numbers': (1,),
+                            },
+                            {
+                                'importer': 'mypackage.utils.brown',
+                                'imported': 'mypackage.yellow.bar',
+                                'line_numbers': (3,),
+                            },
+                        ],
+                        [
+                            {
+                                'importer': 'mypackage.blue.bar',
+                                'imported': 'mypackage.yellow.baz',
+                                'line_numbers': (5,),
+                            },
+                        ],
+                    ],
+                },
+                {
+                    'upstream_module': 'mypackage.green',
+                    'downstream_module': 'mypackage.yellow',
+                    'chains': [
+                        [
+                            {
+                                'importer': 'mypackage.yellow.foo',
+                                'imported': 'mypackage.green.bar',
+                                'line_numbers': (15),
+                            },
+                        ],
+                    ],
+                },
+            ],
+        },
+    )
+
+    contract.render_broken_contract(check)
+
+    settings.PRINTER.pop_and_assert(
+        """
+        mypackage.blue is not allowed to import mypackage.yellow:
+
+        -   mypackage.blue.foo -> mypackage.utils.red (l.16, l.102)
+            mypackage.utils.red -> mypackage.utils.brown (l.1)
+            mypackage.utils.brown -> mypackage.yellow.bar (l.3)
+
+        -   mypackage.blue.bar -> mypackage.yellow.baz (l.5)
+
+
+        mypackage.green is not allowed to import mypackage.yellow:
+
+        -   mypackage.yellow.foo -> mypackage.green.bar (l.15)
+
+
+        """
+    )
