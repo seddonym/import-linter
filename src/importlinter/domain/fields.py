a182cd359c427561df487a978d757f19be349ab7
Warren, Ben <bwarren@eab.com>
2020-09-17 12:00:04 -0400

    Remove regex

--- src/importlinter/domain/fields.py
+++ src/importlinter/domain/fields.py
@@ -117,8 +117,6 @@ class DirectImportField(Field):
     Expects raw data in the form: "mypackage.foo.importer -> mypackage.bar.imported".
     """
 
-    DIRECT_IMPORT_STRING_REGEX = re.compile(r"^([\w\.-]+) -> ([\w\.-]+)$")
-
     def parse(self, raw_data: Union[str, List]) -> DirectImport:
         string = StringField().parse(raw_data)
         importer, _, imported = string.partition(" -> ")
