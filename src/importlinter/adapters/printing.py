3d401c6a2bbddf14fed54cbfb738d241eac2c066
Tapeline <mail@tapeline.dev>
2025-07-11 16:46:54 +0500

    Fix messages being always colored white on Windows

--- src/importlinter/adapters/printing.py
+++ src/importlinter/adapters/printing.py
@@ -13,4 +13,6 @@ class ClickPrinter(Printer):
     def print(
         self, text: str = "", bold: bool = False, color: Optional[str] = None, newline: bool = True
     ) -> None:
-        click.secho(text, bold=bold, fg=color, nl=newline)
+        # A tricky solution to gh-267
+        # Remove when click reaches v9.0.0
+        print(click.style(text, bold=bold, fg=color), end=("\n" if newline else ""))
